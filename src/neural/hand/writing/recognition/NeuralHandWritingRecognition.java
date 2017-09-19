a425fe525d349c875a14e25ebbc0bf42189fa9b9
mathew <mathew@10.59.0.23>
2017-09-19 18:41:19 +0100

    Added Tabs, changed frame startup location.

---
+++ src/neural/hand/writing/recognition/NeuralHandWritingRecognition.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neural.hand.writing.recognition;
+
+/**
+ *
+ * @author mathew
+ */
+public class NeuralHandWritingRecognition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This is where the amazing starts!
+        
+    }
+    
+}
